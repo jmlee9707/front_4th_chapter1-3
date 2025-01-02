50996623cf0a895d318dedbe10f89d163059f5b2
jmlee9707 <jmlee9707@naver.com>
2025-01-02 21:59:00 +0900

    ⚡️ feat : memo code 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+
+import React from "react";
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, useRef } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
-  _equals = shallowEquals,
+  _equals = shallowEquals
 ) {
-  return Component;
+  function MemoComponent(props: P) {
+    const prevProps = useRef<P | null>(null);
+
+    if (prevProps.current === null || !_equals(prevProps.current, props)) {
+      prevProps.current = props;
+      return React.createElement(Component, props);
+    }
+  }
+
+  return MemoComponent;
 }
