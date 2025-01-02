9962cd2fb7f8e7d59262509b224e5ab2f1a5fda9
jmlee9707 <jmlee9707@naver.com>
2025-01-02 20:01:53 +0900

    ⚡️ feat: shallowEqauls 함수 구현

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,26 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  console.log(typeof objA);
+  if (objA === objB) {
+    return true;
+  }
+
+  if (
+    typeof objA !== "object" ||
+    typeof objB !== "object" ||
+    objA === null ||
+    objB === null
+  )
+    return false;
+
+  const aKeys = Object.keys(objA);
+  const bKeys = Object.keys(objB);
+
+  if (aKeys.length !== bKeys.length) return false;
+
+  return aKeys.every(
+    (key) =>
+      bKeys.includes(key) && objA[key as keyof T] === objB[key as keyof T],
+  );
+
+  return true;
 }
